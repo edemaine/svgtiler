ceaead23ce994f52865a192c2eecfabc0cee194e
Erik Demaine <edemaine@mit.edu>
2019-10-03 09:30:12 -0400

    Fix viewBox in polyomino example (fix #55)

--- examples/polyomino/outlines.coffee
+++ examples/polyomino/outlines.coffee
@@ -2,7 +2,7 @@ borderStyle = 'stroke="black" stroke-width="5" stroke-linecap="round"'
 
 (key) -> ->
   s = '''
-    <symbol viewBox="-10 10 20 20" overflowBox="-12.5 -12.5 25 25" style="overflow: visible">
+    <symbol viewBox="-10 -10 20 20" overflowBox="-12.5 -12.5 25 25" style="overflow: visible">
   '''
   if this.neighbor(-1,0).key != key
     s += """<line x1="-10" y1="-10" x2="-10" y2="10" #{borderStyle}/>"""
--- examples/polyomino/outlines.jsx
+++ examples/polyomino/outlines.jsx
@@ -2,7 +2,7 @@ const Border = (props) =>
   <line {...props} stroke="black" stroke-width="5" stroke-linecap="round"/>;
 
 (key) => function() {
-  return <symbol viewBox="-10 10 20 20" overflowBox="-12.5 -12.5 25 25" style="overflow: visible">
+  return <symbol viewBox="-10 -10 20 20" overflowBox="-12.5 -12.5 25 25" style="overflow: visible">
     {this.neighbor(-1,0).key !== key &&
      <Border x1="-10" y1="-10" x2="-10" y2="10"/>}
     {this.neighbor(+1,0).key !== key &&
